e5f7218e352d434ac594d35789d4f77950d8c668
Марина Коновалова <konovalova.owl@gmail.com>
2018-03-15 23:12:06 +0500

    refactoring

--- homestat.py
+++ homestat.py
@@ -1,169 +1,112 @@
 #!/usr/bin/env python3
-from io import StringIO
-from urllib.request import urlopen
-import io
-import lxml.html
+# -*- coding: utf-8 -*-
 import csv
+from io import StringIO
+
+import lxml.html
 
 
 def make_stat(filename):
-    f = urlopen(filename)
-    html = f.read().decode('cp1251')
-    page = lxml.html.parse(StringIO(html))
+    """
+    Функция вычисляет статистику по именам за каждый год с учётом пола.
+    """
+    with open(filename, encoding='cp1251') as f:
+        html = f.read()
+        page = lxml.html.parse(StringIO(html))
     root = page.getroot()
     female_names = set()
-    with io.open('russian_names.csv', encoding='utf-8') as s:
+    with open('russian_names.csv', encoding='utf-8') as s:
         for row in csv.reader(s, delimiter=';'):
             if row[2] == 'Ж':
                 female_names.add(row[1])
     year = None
-    statistic = []  
+    statistic = []
     for i in root.xpath('//tr/td[2]/h3|//tr/td[2]/a'):
         if i.tag == 'h3':
             year = i.text
         else:
-            surname, name = i.text.split(' ')
-            gender = 'Ж' if female_names.__contains__(name) else 'М'
+            surname, name = i.text.split()
+            gender = 'Ж' if name in female_names else 'М'
             statistic.append((year, surname, name, gender))
     return statistic
-    """
-    Функция вычисляет статистику по именам за каждый год с учётом пола.
-    """
-
-
-stat = make_stat('ftp://shannon.usu.edu.ru/python/hw2/home.html')
 
 
 def extract_years(stat):
-    list_years = []
-    for i in stat:
-        if i[0] not in list_years:
-        #if not list_years.__contains__(i[0]):
-            list_years.append(i[0])
-    list_years.sort()
-    return list_years
     """
     Функция принимает на вход вычисленную статистику и выдаёт список годов,
     упорядоченный по возрастанию.
     """
+    list_years = set()
+    for i in stat:
+        list_years.add(i[0])
+    # list_years = {i[0] for i in stat}
+    return sorted(list_years)
 
 
-def extract_general(stat):
-    dict = {}
-    for i in stat:
-        dict[i[2]] = 0
-    for i in stat:
-        dict[i[2]] += 1
-    res_list = tuple([(k, v) for k, v in dict.items()])
-    return sorted(res_list, key=lambda x: x[1], reverse=True)
+def extract_general(stat, sex=None, year=None):
     """
     Функция принимает на вход вычисленную статистику и выдаёт список tuple'ов
     (имя, количество) общей статистики для всех имён.
     Список должен быть отсортирован по убыванию количества.
     """
+    count_dict = {}
+    for i in stat:
+        _year, _name, _sex = i[0], i[2], i[3]
+        if (sex is None or _sex == sex) and (year is None or _year == year):
+            if _name in count_dict:
+                count_dict[_name] += 1
+            else:
+                count_dict[_name] = 1
+    return sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
 
 
 def extract_general_male(stat):
-    dict = {}
-    for i in stat:
-        if i[3] == 'М':
-            dict[i[2]] = 0
-    for i in stat:
-        if i[3] == 'М':
-            dict[i[2]] += 1
-    res_list = tuple([(k, v) for k, v in dict.items()])
-    return sorted(res_list, key=lambda x: x[1], reverse=True)
     """
     Функция принимает на вход вычисленную статистику и выдаёт список tuple'ов
     (имя, количество) общей статистики для имён мальчиков.
     Список должен быть отсортирован по убыванию количества.
     """
+    return extract_general(stat, sex='М')
 
 
 def extract_general_female(stat):
-    dict = {}
-    for i in stat:
-        if i[3] == 'Ж':
-            dict[i[2]] = 0
-    for i in stat:
-        if i[3] == 'Ж':
-            dict[i[2]] += 1
-    res_list = tuple([(k, v) for k, v in dict.items()])
-    return sorted(res_list, key=lambda x: x[1], reverse=True)
     """
     Функция принимает на вход вычисленную статистику и выдаёт список tuple'ов
     (имя, количество) общей статистики для имён девочек.
     Список должен быть отсортирован по убыванию количества.
     """
+    return extract_general(stat, sex='Ж')
 
 
 def extract_year(stat, year):
-    dict = {}
-    for i in stat:
-        if i[0] == year:
-            dict[i[2]] = 0
-    for i in stat:
-        if i[0] == year:
-            dict[i[2]] += 1
-    res_list = tuple([(k, v) for k, v in dict.items()])
-    return sorted(res_list, key=lambda x: x[1], reverse=True)
-
-
     """
     Функция принимает на вход вычисленную статистику и год.
     Результат — список tuple'ов (имя, количество) общей статистики для всех
     имён в указанном году.
     Список должен быть отсортирован по убыванию количества.
     """
+    return extract_general(stat, year=year)
 
 
 def extract_year_male(stat, year):
-    dict = {}
-    for i in stat:
-        if i[0] == year:
-                if i[3] == 'М':
-                    dict[i[2]] = 0
-    for i in stat:
-        if i[0] == year:
-                if i[3] == 'М':
-                    dict[i[2]] += 1
-    res_list = tuple([(k, v) for k, v in dict.items()])
-    return sorted(res_list, key=lambda x: x[1], reverse=True)
     """
-    Функция принимает на вход вычисленную статистику и год.
-    Результат — список tuple'ов (имя, количество) общей статистики для всех
-    имён мальчиков в указанном году.
-    Список должен быть отсортирован по убыванию количества.
-    """
+   Функция принимает на вход вычисленную статистику и год.
+   Результат — список tuple'ов (имя, количество) общей статистики для всех
+   имён мальчиков в указанном году.
+   Список должен быть отсортирован по убыванию количества.
+   """
+    return extract_general(stat, sex='М', year=year)
 
 
 def extract_year_female(stat, year):
-    dict = {}
-    for i in stat:
-        if i[0] == year:
-                if i[3] == 'Ж':
-                    dict[i[2]] = 0
-    for i in stat:
-        if i[0] == year:
-            if i[3] == 'Ж':
-                dict[i[2]] += 1
-    res_list = tuple([(k, v) for k, v in dict.items()])
-    return sorted(res_list, key=lambda x: x[1], reverse=True)
     """
     Функция принимает на вход вычисленную статистику и год.
     Результат — список tuple'ов (имя, количество) общей статистики для всех
     имён девочек в указанном году.
     Список должен быть отсортирован по убыванию количества.
     """
+    return extract_general(stat, sex='Ж', year=year)
 
 
 if __name__ == '__main__':
-    print(make_stat('ftp://shannon.usu.edu.ru/python/hw2/home.html'))
-    print(extract_years(stat))
-   # print(extract_general(stat))
-   # print(extract_general_male(stat))
-   # print(extract_general_female(stat))
-   # print(extract_year(stat, '2009'))
-   # print(extract_year_male(stat, '2009'))
-   # print(extract_year_female(stat, '2009'))
-
+    print(make_stat('home.html'))
